578b31793910448cb3a7415d220d39fa8de6415a
Moufid Larbi <moufid4@gmail.com>
2015-06-19 22:21:35 +0100

    added ui to create new teams

--- client/views/teams.js
+++ client/views/teams.js
@@ -1,8 +1,23 @@
 Template.teams.helpers({
-	isCreatingTeam: function(){
-		return Session.get('isCreatingTeam');
-	},
-	teams: function(){
-		return Teams.find();
-	}
+  isCreatingTeam: function(){
+    return Session.get('isCreatingTeam');
+  },
+  teams: function(){
+    return Teams.find();
+  } 
+});
+
+Template.teams.events({
+  'click a.create': function(e, tpl){
+    Session.set('isCreatingTeam', true);
+  },
+
+  'click a.cancel': function(e, tpl){
+    Session.set('isCreatingTeam', false);
+  },
+
+  'submit form.create-team': function(e, tpl){
+    var teamName = $('input[name=name]').val();
+    Teams.insert({name: teamName});
+  }
 });
