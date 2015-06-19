db33125919491a0338e20d66d1f8a05d46db089a
Moufid Larbi <moufid4@gmail.com>
2015-06-19 23:19:17 +0100

    added remove button

--- client/views/teams.js
+++ client/views/teams.js
@@ -18,9 +18,25 @@ Template.teams.events({
     Session.set('isCreatingTeam', false);
   },
 
-  'submit form.create-team': function(e, tpl){
-    e.preventDefault();
-    var teamName = $('input[name=name]').val();
-    Teams.insert({name: teamName});
+   'submit form.create-team': function(e, tpl){
+   	e.preventDefault();
+	var teamName = tpl.$('input[name=name]').val();
+	Teams.insert({name: teamName}, function(error, _id){
+	  if(error){
+	    alert(error);
+	    Session.set('isCreatingTeam', true);
+	    Tracker.afterFlush(function(){
+	      tpl.$('input[name=name]').val(teamName);
+	    });
+	  }
+	});
+	Session.set('isCreatingTeam', false);
+	},
+
+	'click a.remove': function(e, tpl){
+    	e.preventDefault();
+    	Teams.remove(this._id);
   }
+
 });
+
