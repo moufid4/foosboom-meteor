e6a2d174c1deb84aff83863057c99acb1c608434
Moufid Larbi <moufid4@gmail.com>
2015-06-22 22:29:09 +0100

    added users accounts

--- client/views/team.js
+++ client/views/team.js
@@ -13,20 +13,28 @@ Template.team.events({
   "submit form.form-edit": function(e, tpl){
     e.preventDefault();
  
-    var teamName = tpl.$('input[name="name"]').val();
+    var teamName = tpl.$("input[name='name']").val();
+    var self = this;
+
     if(teamName.length){
-      Teams.update(this._id, {$set: {name: teamName}});
+      Teams.update(this._id, {$set: {name: teamName}}, function(error){
+
+        if(!error){
+
+          var games = Games.find({_id: {$in: self.gameIds}});
+          if(games.count()){
+            _(games.fetch()).each(function(game){
+              var team = _(game.teams).findWhere({id: self._id});
+              if(team != null){
+                team.name = teamName;
+                Games.update({_id: game._id}, {$set: {teams: game.teams}})
+              }
+            });
+          }
+        }
+      });
+
       Session.set('editedTeamId', null);
     }
   },
- 
-  "click a.cancel": function(e, tpl){
-    e.preventDefault();
-    Session.set('editedTeamId', null);
-  },
- 
-  'click a.remove': function(e, tpl){
-    e.preventDefault();
-    Teams.remove(this._id);
-  }
 });
