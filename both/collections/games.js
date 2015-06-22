ce7327b0caf3be83d05b0cc39ecf00e27d71e422
Moufid Larbi <moufid4@gmail.com>
2015-06-23 00:45:13 +0100

    added a remote function to add new games

--- both/collections/games.js
+++ both/collections/games.js
@@ -12,3 +12,36 @@ Games.allow({
 	},
 	fetch: ['ownerId']
 });
+
+Meteor.methods({
+  gamesInsert: function(teamOneId, teamTwoId){
+    var teamOne = Teams.findOne({_id: teamOneId});
+    var teamTwo = Teams.findOne({_id: teamTwoId});
+ 
+    var teamOneData = {
+      _id: teamOne._id,
+      name: teamOne.name,
+      score: 0
+    };
+
+    var teamTwoData = {
+      _id: teamTwo._id,
+      name: teamTwo.name,
+      score: 0
+    };
+
+    var game = {
+      ownerId: Meteor.userId(),
+      createdAt: new Date(),
+      teams: [teamOneData, teamTwoData],
+      completed: false
+    };
+ 
+    var gameId = Games.insert(game);
+ 
+    Teams.update({_id: teamOneData._id}, {$addToSet: { gameIds: gameId}});
+    Teams.update({_id: teamTwoData._id}, {$addToSet: { gameIds: gameId}});
+ 
+    return gameId;
+  }
+});
