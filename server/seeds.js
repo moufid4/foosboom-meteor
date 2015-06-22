28d25b180b879de12a9cee88f7fabbfd770cbcb4
Moufid Larbi <moufid4@gmail.com>
2015-06-22 23:06:54 +0100

    updated seed data for accounts

--- server/seeds.js
+++ server/seeds.js
@@ -1,35 +1,48 @@
 Meteor.startup(function () {
-	if (Teams.find().count() === 0) {
-		[
-			{name:"Barcelona",
-		        gameIds: []
-			},
-			{name: "Real Madrid",
-				gameIds: []
-			},
-			{name: "Mouloudia",
-		        gameIds: []
-			}
-		].forEach(function(team){
-			Teams.insert(team);
-		});
-		 
+  var dummyUserEmail = 'test@test.com'
+
+  if (Meteor.users.find({"emails.address": dummyUserEmail}).count() == 0){
+
+    var ownerId = Accounts.createUser({
+      email: dummyUserEmail,
+      password: 'moufid'
+    });
+  
+    [
+      {
+        name: "Barcelona",
+        gameIds: [],
+        ownerId: ownerId
+      },
+      {
+        name: "Real Madrid",
+        gameIds: [],
+        ownerId: ownerId
+      },
+      {
+        name: "Matt's team",
+        gameIds: [],
+        ownerId: ownerId
+      }
+    ].forEach(function(team){
+      Teams.insert(team);
+    });
+
     var team1 = Teams.find().fetch()[0];
     var team2 = Teams.find().fetch()[1];
 
     var game = {
       completed: false,
-      createdAt: new Date(),
+      createdAt: new Date,
       teams: [
         {name: team1.name, _id: team1._id, score: 0},
         {name: team2.name, _id: team2._id, score: 0}
       ]
     };
    
-    Games.insert(game);
+    gameId = Games.insert(game);
 
     Teams.update({_id: team1._id}, {$addToSet: { gameIds: gameId}});
     Teams.update({_id: team2._id}, {$addToSet: { gameIds: gameId}});
-  
-	}
+  }
 });
